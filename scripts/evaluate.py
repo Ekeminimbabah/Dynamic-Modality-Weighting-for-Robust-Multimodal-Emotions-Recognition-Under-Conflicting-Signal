6362233f477ccc51004090190a6520f89497e92e
Ekemini <2510856@leedstrinity.ac.uk>
2026-09-03 21:25:40 +0100

    Update evaluation script to incorperate comments

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,3 +1,4 @@
+
 import sys
 from pathlib import Path
 
@@ -8,7 +9,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-
+# Import necessary libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
